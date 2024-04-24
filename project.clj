2f59a8c90657c5bbc5b3d7c0a07e93e4f6b11bfb
ejschoen <ejschoen@i2kconnect.com>
2024-04-24 08:46:56 -0500

    Update test and clojure dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.ejschoen/ring-correlation-id "0.6.0-SNAPSHOT"
+(defproject org.clojars.ejschoen/ring-correlation-id "0.6.0-clj-11-SNAPSHOT"
   :description "Correlation ID tracing for distributed systems using ring and clj-http"
   :url "https://github.com/ejschoen/ring-correlation-id.git"
   :license {:name "Eclipse Public License"
@@ -6,13 +6,15 @@
   :dependencies []
   :plugins [[lein-eftest "0.5.9"]]
   :eftest {:multithread? false}
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.11.1"]
                                   [org.clojure/core.cache "1.0.207"]
                                   [ring "1.6.3"]
                                   [com.taoensso/encore "2.126.2"]
                                   [com.taoensso/timbre "4.10.0"]
                                   [clj-http "3.10.0"]
                                   [clj-http-fake "1.0.3"]
-                                  [org.clojars.ejschoen/clj-telemetry "0.3.0-SNAPSHOT" ]
+                                  [org.clojars.ejschoen/clj-telemetry "0.3.0-SNAPSHOT"
+                                   :exclusions [org.clojure/clojure]]
                                   [cheshire "5.9.0"]
-                                  ]}})
+                                  ]}
+             :test {:dependencies [[io.grpc/grpc-all "1.63.0"]]}})
